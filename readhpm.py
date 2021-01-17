8cb7dc7a8ff21c817af05ac0c549405c4fd3713c
reps <marco@reps.cc>
2021-01-17 23:14:51 +0100

    Update readhpm.py

--- readhpm.py
+++ readhpm.py
@@ -1,8 +1,13 @@
 import serial
 import binascii
+from datetime import datetime
+
 ser = serial.Serial('/dev/ttyUSB1', 921600)
 
 f = open("10khz_short_circuit.csv", "w")
+
+now = datetime.now()
+
 
 while True:
     char = ser.read_until(b'\xa0\r', size=6)
@@ -10,4 +15,4 @@ while True:
         number = int(binascii.hexlify(char[:-2]), 16)
     except Exception:
         pass
-    f.write(str(number)+",")
+    f.write(now = datetime.now()+","str(number)+"\n")
