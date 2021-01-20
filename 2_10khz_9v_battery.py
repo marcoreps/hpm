39e31fce3e0548e7fc42421d9a146a0b5e81c8a7
reps <marco@reps.cc>
2021-01-20 18:02:56 +0100

    hjk

--- 2_10khz_9v_battery.py
+++ 2_10khz_9v_battery.py
@@ -1,14 +1,14 @@
 import serial
+import binascii
 from datetime import datetime
 import threading
 import time
-import binascii
 
 
 hpm1_dev = 'COM6'
 hpm1_baud = 921600
 
-f = open("2_10khz_short_circuit.csv", "w")
+f = open("2_10khz_9v_battery.csv", "w")
 f.write("time,counts\n")
 
 buffer_1 = bytearray()
@@ -18,9 +18,8 @@ lock = threading.Lock()
 def readserial(dev, baud, buf, lock):
     s = serial.Serial(dev, baud)
     while True:
-        if s.in_waiting > 2048:
-            print("reading new buffer")
-            reading=s.read(2048)
+        if s.in_waiting > 1024:
+            reading=s.read(1024)
             with lock:
                 buf.extend(reading)
         
@@ -30,20 +29,19 @@ thread_1.start()
 
 while True:
 
-    if(len(buffer_1)>5):
-        if(buffer_1[4]==160 and buffer_1[5]==13):
-            number = int.from_bytes(buffer_1[:4], byteorder='big', signed=False)
-            with lock:
-                del buffer_1[:6]
+
+    pos=buffer_1.find(b'\xa0\r')
+    if(pos>=0):
+        if(pos>0):
+            number = int(binascii.hexlify(buffer_1[:pos]), 16)
+        with lock:
+            del buffer_1[:pos+2]
+        if(pos==4):
             dateTimeObj = datetime.now()
             timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
             f.write(timestampStr+","+str(number)+"\n")
         else:
-            dateTimeObj = datetime.now()
-            timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
-            print(timestampStr+" error")
-            with lock:
-                del buffer_1[0]
+            print("read error")
 
 
 
